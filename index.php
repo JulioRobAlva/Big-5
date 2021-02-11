30a1dde4223362800d9035e8201091055d7f9f8f
JulioRobAlva <39749815+JulioRobAlva@users.noreply.github.com>
2021-02-11 04:05:15 -0600

    Final...

--- index.php
+++ index.php
@@ -1,20 +1,55 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/superhero/bootstrap.min.css" integrity="sha384-HnTY+mLT0stQlOwD3wcAzSVAZbrBp141qwfR4WfTqVQKSgmcgzk+oP0ieIyrxiFO" crossorigin="anonymous">
+<!-- JULIOROBALVA -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cyborg/bootstrap.min.css" integrity="sha384-nEnU7Ae+3lD52AK+RGNzgieBWMnEfgTbRHIwEvp1XXPdqdO6uLTd/NwXbzboqjc2" crossorigin="anonymous">
 <script src="https://kit.fontawesome.com/f7e67d00d0.js" crossorigin="anonymous"></script>
+<!-- Bootswatch cnd Cyborg , -->
 
 
-<i class="fas fa-bullseye"></i>
-<form method="POST" action="ordenar.php" >
-<div>
-<input type="text" name="numero1" id="numero1">
-<input type="text" name="numero2" id="numero2">
-<input type="text" name="numero3" id="numero3">
-<input type="text" name="numero4" id="numero4">
-<input type="text" name="numero5" id="numero5">
+
+
+<!-- Nav -->
+
+<div class="jumbotron">
+  <h1 align="center" class="display-3">Big 5</h1>
+  <p align="center" class="lead">Diseñado para ingresar 5 numeros y ordenarlos de  <a href="#">Mayor a Menor</a> junto a su  <a href="#">Promedio</a> </p>
+  <hr class="my-4">
+  <div class="progress">
+  <div class="progress-bar bg-success" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
 </div>
-<button type="submit" <i class="fas fa-bullseye"></i> Ordenar</button>
+<p align="center">Puedes ingresar los numeros que quieras en <strong>caja de texto</strong> y darle al<strong>boton</strong> para obtener los numeros de mayor a menor  .</p>
+</div>
+
+
+
+<!-- Inicio de Procesos -->
+<form method="POST" action="ordenar.php">
+
+<table class="table table-hover">
+  <thead>
+  <tr class="table-light">
+      <th scope="col">Numero 1</th>
+      <th scope="col">Numero 2</th>
+      <th scope="col">Numero 3</th>
+      <th scope="col">Numero 4</th>
+      <th scope="col">Numero 5</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <tr class="table-active">
+      <td><input type="number" min="0" class="form-control"type="text" name="numero1" id="numero1"></td>
+      <td><input type="number" min="0"class="form-control" type="text" name="numero2" id="numero2"></td>
+      <td><input type="number" min="0" class="form-control" type="text" name="numero3" id="numero3"></td>
+      <td><input type="number" min="0" class="form-control" type="text" name="numero4" id="numero4"></td>
+      <td><input type="number" min="0" class="form-control" type="text" name="numero5" id="numero5"></td>
+    </tr>
+  </tbody>
+</table>
+<button type="submit"  class="btn btn-primary btn-lg btn-block" >Ejecutar </button>
 
 </form>
 
-
-
-
+<div class="alert alert-dismissible alert-warning">
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+  <h4 class="alert-heading">Atencion!</h4>
+  <p class="mb-0">Solo puedes ingresar <a href="#" class="alert-link">NUMEROS</a> ya que otra forma de caracter no se podra ingresar.</p>
+</div>
--- ordenar.php
+++ ordenar.php
@@ -1,4 +1,21 @@
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/superhero/bootstrap.min.css" integrity="sha384-HnTY+mLT0stQlOwD3wcAzSVAZbrBp141qwfR4WfTqVQKSgmcgzk+oP0ieIyrxiFO" crossorigin="anonymous">
+<!-- JULIOROBALVA -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cyborg/bootstrap.min.css" integrity="sha384-nEnU7Ae+3lD52AK+RGNzgieBWMnEfgTbRHIwEvp1XXPdqdO6uLTd/NwXbzboqjc2" crossorigin="anonymous">
+<script src="https://kit.fontawesome.com/f7e67d00d0.js" crossorigin="anonymous"></script>
+<!-- Bootswatch cnd Cyborg , -->
+
+
+
+<div class="jumbotron">
+  <h1 align="center" class="display-3">Big 5</h1>
+  <p align="center" class="lead">Diseñado para ingresar 5 numeros y ordenarlos de  <a href="#">Mayor a Menor</a> junto a su  <a href="#">Promedio</a> </p>
+  <hr class="my-4">
+  <div class="progress">
+  <div class="progress-bar bg-success" role="progressbar" style="width: 100%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+</div>
+</div>
+
+  <h1 align="center" class="display-3">Numeros Ordenados</h1>
+  <p align="center" class="lead">  <a href="#">Mayor a Menor</a> </p>
 
 <?php
 
@@ -11,22 +28,19 @@ $numero5 = intval($_POST["numero5"]);
 $orden = array($numero1, $numero2, $numero3,$numero4,$numero5);
 rsort($orden);
 
+
 $promedio= 0;
-
-
 foreach ($orden as $or){
   //  print($or);
    // echo "<br>";
 ?>
-<h2><?php print($or); ?></h2>
+<h3 align="center"><?php print($or); ?></h3>
 
 <?php
     $promedio = $promedio+ $or;
 }
-
 $promtotal = $promedio / 5;
 //print($promtotal);
-
 ?>
 
-<h1>promedio : <?php print($promtotal); ?></h1>
+<h3 align="center"><a href="#">Promedio </a><?php print($promtotal); ?></h3>
